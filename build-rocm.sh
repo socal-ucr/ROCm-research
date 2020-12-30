94295ecb22d48f0e559d9e2d2aa2c5491f09bf81
Marcus Chow <mchow009@ucr.edu>
2020-12-30 19:36:16 +0000

    Update to rocm 4.0.0 Change llvm repo fixed build script

--- build-rocm.sh
+++ build-rocm.sh
@@ -12,63 +12,57 @@ make -j
 make install
 cd ${cwd}
 
+CC_DIR=${INSTALL_DIR}/rocm/llvm/bin/clang
+CXX_DIR=${INSTALL_DIR}/rocm/llvm/bin/clang++
+
 cd rocm-cmake
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm ..
+CC=${CC_DIR}CXX=${CXX_DIR} cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm ..
 cmake --build . --target install
 cd ${cwd}
 
-cd ROCT-Thunk-Interface
+cd ROCm-Device-Libs
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm ..
-make package
-make install
-cd ${cwd}
-
-cd ROCR-Runtime/src
-mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm -DHSAKMT_INC_PATH=${INSTALL_DIR}/rocm/include -DHSAKMT_LIB_PATH=${INSTALL_DIR}/rocm/lib ..
-make -j 
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/rocm/llvm -DLLVM_DIR=${INSTALL_DIR}/rocm/llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/rocdl ..
+make -j
 make install
 cd ${cwd}
 
 cd rocminfo
 mkdir build && cd build
-cmake -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/rocm  -DCMAKE_INSTALL_BINDIR=${INSTALL_DIR}/rocm/bin ..
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/rocm  -DCMAKE_INSTALL_BINDIR=${INSTALL_DIR}/rocm/bin ..
 make
 make install
 cd ${cwd}
 
-cd ROCm-Device-Libs
+cd ROCT-Thunk-Interface
 mkdir build && cd build
-CC=${INSTALL_DIR}/rocm/llvm/bin/clang CXX=${INSTALL_DIR}/rocm/llvm/bin/clang++ cmake -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/rocm/llvm -DLLVM_DIR=${INSTALL_DIR}/rocm/llvm -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/rocdl ..
-make -j
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm ..
+make install
+cd ${cwd}
+
+cd ROCR-Runtime/src
+mkdir build && cd build
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/include;${INSTALL_DIR}/rocm/lib" ..
+make -j 
 make install
 cd ${cwd}
 
 cd ROCm-CompilerSupport/lib/comgr
 mkdir build && cd build
-cmake -DCMAKE_BUILD_TYPE=Release  -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/llvm;${INSTALL_DIR}/rocm/rocdl" -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/comgr ..
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_BUILD_TYPE=Release  -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/llvm;${INSTALL_DIR}/rocm/rocdl" -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/comgr ..
 cd ${cwd}
 
 ROCclr_DIR="$(readlink -f ROCclr)"
 OPENCL_DIR="$(readlink -f ROCm-OpenCL-Runtime)"
 cd ${ROCclr_DIR}
 mkdir build && cd build
-cmake -DOPENCL_DIR=${OPENCL_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/rocclr ..
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/comgr" -DOPENCL_DIR=${OPENCL_DIR} -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/rocclr ..
 make -j
 make install
-
-cd ${OPENCL_DIR}
-mkdir build && cd build
-cmake -DUSE_COMGR_LIBRARY=ON -DCMAKE_PREFIX_PATH="${ROCclr_DIR}/build;${INSTALL_DIR}/rocm/" -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/opencl ..
-make -j
-make install
-cd ${cwd}
-
 
 cd HIP
 mkdir build && cd build
-cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/hip -DHIP_COMPILER=clang -DCMAKE_BUILD_TYPE=Release -DHIP_PLATFORM=rocclr -DOPENCL_DIR=${INSTALL_DIR}/rocm/opencl -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/rocclr;${INSTALL_DIR}/rocm/lib/cmake/hsa-runtime64/;${INSTALL_DIR}/rocm/comgr" -DHSA_PATH=${INSTALL_DIR}/rocm/hsa -DROCM_PATH=${INSTALL_DIR}/rocm ..
+CC=${CC_DIR} CXX=${CXX_DIR} cmake -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/hip -DHIP_COMPILER=clang -DCMAKE_BUILD_TYPE=Release -DHIP_PLATFORM=rocclr -DOPENCL_DIR=${INSTALL_DIR}/rocm/opencl -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/llvm;${INSTALL_DIR}/rocm/rocclr;${INSTALL_DIR}/rocm/lib/cmake/hsa-runtime64/;${INSTALL_DIR}/rocm/comgr" -DHSA_PATH=${INSTALL_DIR}/rocm/hsa -DROCM_PATH=${INSTALL_DIR}/rocm ..
 make -j
 make install
