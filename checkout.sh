38cb0883fcbaa1304adc8fd902f5c050addb6e98
Ali Jahan <ajaha004@ucr.edu>
2021-06-14 22:20:04 +0000

    branch names are defined at the top. rocprofiler

--- checkout.sh
+++ checkout.sh
@@ -2,57 +2,75 @@
 
 cwd=$(pwd)
 
+vHIP="rocm-4.1.x"
+vROCclr="rocm-4.1.x"
+vROCR_Runtime="rocm-4.1.x"
+vROCT_Thunk_Interface="roc-4.1.x"
+vROCm_CompilerSupport="roc-4.1.x"
+vROCm_Device_Libs="roc-4.1.x"
+vROCm_OpenCL_Runtime="rocm-4.1.x"
+vllvm_project="roc-4.1.x"
+vrocm_cmake="roc-4.0.x"
+vrocm_smi_lib="release/rocm-rel-4.1"
+vrocminfo="release/rocm-rel-4.1"
+vrocprofiler="rocm-4.1.x"
+
 #HIP
 cd HIP
-git checkout rocm-4.1.x
+git checkout ${vHIP}
 cd ${cwd}
 
 #ROCclr
 cd ROCclr
-git checkout rocm-4.1.x
+git checkout ${vROCclr}
 cd ${cwd}
 
 #ROCR-Runtime
 cd ROCR-Runtime
-git checkout rocm-4.1.x
+git checkout ${vROCR_Runtime}
 cd ${cwd}
 
 #ROCT-Thunk-Interface
 cd ROCT-Thunk-Interface
-git checkout roc-4.1.x
+git checkout ${vROCT_Thunk_Interface}
 cd ${cwd}
 
 #ROCm-CompilerSupport
 cd ROCm-CompilerSupport
-git checkout roc-4.1.x
+git checkout ${vROCm_CompilerSupport}
 cd ${cwd}
 
 #ROCm-Device-Libs
 cd ROCm-Device-Libs
-git checkout roc-4.1.x
+git checkout ${vROCm_Device_Libs}
 cd ${cwd}
 
 #ROCm-OpenCL-Runtime
 cd ROCm-OpenCL-Runtime
-git checkout rocm-4.1.x
+git checkout ${vROCm_OpenCL_Runtime}
 cd ${cwd}
 
 #llvm-project
 cd llvm-project
-git checkout roc-4.1.x
+git checkout ${vllvm_project}
 cd ${cwd}
 
 #rocm-cmake
 cd rocm-cmake
-git checkout roc-4.0.x
+git checkout ${vrocm_cmake}
 cd ${cwd}
 
 #rocm_smi_lib
 cd rocm_smi_lib
-git checkout release/rocm-rel-4.1
+git checkout ${vrocm_smi_lib}
 cd ${cwd}
 
 #rocminfo
 cd rocminfo
-git checkout release/rocm-rel-4.1
+git checkout ${vrocminfo}
+cd ${cwd}
+
+#rocprofiler
+cd rocprofiler
+git checkout ${vrocprofiler}
 cd ${cwd}
