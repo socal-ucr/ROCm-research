44a012454680223756045829c8f3496ee840591f
Marcus Chow <mchow009@ucr.edu>
2022-07-15 19:06:31 +0000

    Upgrade to rocm-5.2.0

--- build-rocm.sh
+++ build-rocm.sh
@@ -9,7 +9,7 @@ trap 'if [ $? -ne 0 ]; then echo "\"${last_command}\" command failed with exit c
 
 INSTALL_DIR=${HOME}/.opt
 cwd=$(pwd)
-CMAKE=/usr/local/bin/cmake
+CMAKE=/usr/bin/cmake
 
 CC_DIR=${INSTALL_DIR}/rocm/llvm/bin/clang
 CXX_DIR=${INSTALL_DIR}/rocm/llvm/bin/clang++
@@ -63,6 +63,14 @@ make -j
 make install
 cd ${cwd}
 
+cd rocminfo
+rm -rf build
+mkdir build && cd build
+CC=${CC_DIR} CXX=${CXX_DIR} ${CMAKE} -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/rocm  -DCMAKE_INSTALL_BINDIR=${INSTALL_DIR}/rocm/bin -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm ..
+make
+make install
+cd ${cwd}
+
 HIPAMD_DIR="$(readlink -f hipamd)"
 HIP_DIR="$(readlink -f HIP)"
 ROCclr_DIR="$(readlink -f ROCclr)"
@@ -72,14 +80,6 @@ cd ${HIPAMD_DIR}
 rm -rf build; mkdir -p build; cd build
 CC=${CC_DIR} CXX=${CXX_DIR} ${CMAKE} -DHIP_COMMON_DIR=$HIP_DIR -DAMD_OPENCL_PATH=$OPENCL_DIR -DROCCLR_PATH=$ROCCLR_DIR -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR}/rocm/hip -DHIP_COMPILER=clang -DHIP_CLANG_PATH=${INSTALL_DIR}/rocm/llvm/bin -DCMAKE_BUILD_TYPE=Release -DHIP_PLATFORM=rocclr -DOPENCL_DIR=${INSTALL_DIR}/rocm/opencl -DCMAKE_PREFIX_PATH="${INSTALL_DIR}/rocm/llvm;${ROCclr_DIR}/build;${INSTALL_DIR}/rocm/lib/cmake/hsa-runtime64;${INSTALL_DIR}/rocm/comgr;${INSTALL_DIR}/rocm" -DHSA_PATH=${INSTALL_DIR}/rocm/hsa -DROCM_PATH=${INSTALL_DIR}/rocm -DDEVICE_LIB_PATH=${INSTALL_PATH}/rocm/rocdl -DCMAKE_HIP_ARCHITECTURES=gfx906 ..
 make -j 
-make install
-cd ${cwd}
-
-cd rocminfo
-rm -rf build
-mkdir build && cd build
-CC=${CC_DIR} CXX=${CXX_DIR} ${CMAKE} -DCMAKE_PREFIX_PATH=${INSTALL_DIR}/rocm  -DCMAKE_INSTALL_BINDIR=${INSTALL_DIR}/rocm/bin ..
-make
 make install
 cd ${cwd}
 
@@ -93,9 +93,9 @@ cd ${cwd}
 
 if [ ! -d "${INSTALL_DIR}/rocm/.info" ]; then
     mkdir ${INSTALL_DIR}/rocm/.info
-    echo 4.5.0-56 > ${INSTALL_DIR}/rocm/.info/version
-    echo 4.5.0-56 > ${INSTALL_DIR}/rocm/.info/version-dev
-    echo 4.5.0-56 > ${INSTALL_DIR}/rocm/.info/version-utils
+    echo 5.2.0-65 > ${INSTALL_DIR}/rocm/.info/version
+    echo 5.2.0-65 > ${INSTALL_DIR}/rocm/.info/version-dev
+    echo 5.2.0-65 > ${INSTALL_DIR}/rocm/.info/version-utils
 fi
 
 if [ ! -d "${INSTALL_DIR}/rocm/include/hip" ]; then
@@ -110,15 +110,11 @@ if [ ! -f "${INSTALL_DIR}/rocm/lib/libamdhip64.so" ]; then
     echo "LINKING hip/lib/libamdhip64.so"
     ln -s ${INSTALL_DIR}/rocm/hip/lib/libamdhip64.so ${INSTALL_DIR}/rocm/lib/.
 fi
-if [ ! -f "${INSTALL_DIR}/rocm/lib/libamd_comgr.so.2.1" ]; then
+if [ ! -f "${INSTALL_DIR}/rocm/lib/libamd_comgr.so.2.4" ]; then
     echo "LINKING hip/lib/libamd_comgr.so"
-    ln -s ${INSTALL_DIR}/rocm/comgr/lib/libamd_comgr.so.2.1 ${INSTALL_DIR}/rocm/lib/.
+    ln -s ${INSTALL_DIR}/rocm/comgr/lib/libamd_comgr.so.2.4 ${INSTALL_DIR}/rocm/lib/.
     ln -s ${INSTALL_DIR}/rocm/comgr/lib/libamd_comgr.so.2 ${INSTALL_DIR}/rocm/lib/.
     ln -s ${INSTALL_DIR}/rocm/comgr/lib/libamd_comgr.so ${INSTALL_DIR}/rocm/lib/.
-fi
-if [ ! -f "${INSTALL_DIR}/rocm/lib/libhiprand.so" ]; then
-     echo "LINKING hip/lib/libhiprand.so"
-     ln -s ${INSTALL_DIR}/rocm/hiprand/lib/* ${INSTALL_DIR}/rocm/lib/.
 fi
 if [ ! -d "${INSTALL_DIR}/rocm/amdgcn" ]; then
     echo "LINKING rocm/amdgcn"
